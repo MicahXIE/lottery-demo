da875fdc32b1c67da2a89f01af7c0c7700270a66
haoxiang peng <kennyshallnotdie@gmail.com>
2018-06-03 21:46:58 +0800

    it works!

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+   'YOUR mnemonic',
+    'https://rinkeby.infura.io/YOUR KEY'
+   );
+   
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+
+    console.log('Attempting to deploy from account', accounts[0]);
+     const result = await new web3.eth.Contract(JSON.parse(interface))  // to record where the contract ends up to
+    .deploy({ data: '0x' + bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+     console.log(interface);
+     console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
